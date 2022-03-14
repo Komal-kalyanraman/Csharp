5f148bac78c2bcccd30d825b5b55188a0a9a8928
Komala Kalyanraman Yerra (SX/EDS-DL1) <Yerra.KomalaKalyanraman@in.bosch.com>
2022-03-14 11:54:07 +0530

    base files

---
+++ SWD-Day6/Http.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD_Day6
+{
+    public class Http
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+
+        /// <summary>
+        /// Call a REST API with GET.
+        /// <summary>
+        /// <param name="url">URL for the REST API</param>
+        /// <returns>Returns the stringified form of the return value of the API</returns>
+        public async Task<string> Get(string url)
+        {
+            return await Call("GET", url, new Dictionary<string, string>());
+        }
+
+
+        /// <summary>
+        /// Call a REST API with a POST
+        /// <summary>
+        /// <param name="url">URL for the REST API</param>
+        /// <param name="data">POST data</param>
+        /// <returns>Returns the stringified form of the return value of the API</returns>
+        public async Task<string> Post(string url, Dictionary<string, string> data)
+        {
+            return await Call("POST", url, data);
+        }
+
+
+        /// <summary>
+        /// Call a REST API with a PUT
+        /// <summary>
+        /// <param name="url">URL for the REST API</param>
+        /// <param name="data">PUT data</param>
+        /// <returns>Returns the stringified form of the return value of the API</returns>
+        public async Task<string> Put(string url, Dictionary<string, string> data)
+        {
+            //string key = ToString().data[0];
+            //Console.WriteLine("key: value", data.Keys, data.Values);
+            return await Call("PUT", url, data);
+        }
+
+
+        /// <summary>
+        /// Call a REST API with the specified method and URL
+        /// <summary>
+        /// <param name="method">Method for API - POST, GET or PUT</param>
+        /// <param name="url">URL for the REST API</param>
+        /// <param name="data">Query data for POST, PUT and GET in the form of a dictionary</param>
+        /// <returns>Returns the stringified form of the return value of the API</returns>
+        private async Task<string> Call(string method, string url, Dictionary<string, string> data)
+        {
+            var content = new FormUrlEncodedContent(data);
+            string retval = "";
+            HttpResponseMessage response;
+
+
+            switch (method)
+            {
+                case "POST":
+                    response = await client.PostAsync(url, content);
+                    retval = await response.Content.ReadAsStringAsync();
+                    break;
+
+
+                case "GET":
+                    // Get does not use the data sent to us. The query parameters will have
+                    // to be given along with the url: http://<server>:<port>/api?param1=hello&param2=world
+                    retval = await client.GetStringAsync(url);
+                    break;
+
+
+                case "PUT":
+                    response = await client.PutAsync(url, content);
+                    retval = await response.Content.ReadAsStringAsync();
+                    break;
+
+
+                default:
+                    retval = "ERROR";
+                    break;
+            }
+
+            return retval;
+        }
+    }
+}
---
+++ SWD-Day6/Program.cs
@@ -0,0 +1,17 @@
+﻿using SWD_Day6;
+using System.Collections.Generic;
+
+Http api = new Http();
+
+// Switch on the light
+Dictionary<string, string> data = new Dictionary<string, string>
+{
+    {"address", "Socket"},
+    {"state", "ON"}
+};
+
+string ret = await api.Put("http://192.168.1.10:8080/shapi/control", data);
+//string ret = await api.Put("http://localhost:8080/shapi/control", data);
+Console.WriteLine(ret);
+
+//Console.WriteLine("hello-world");
