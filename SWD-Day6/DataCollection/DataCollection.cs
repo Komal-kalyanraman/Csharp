53ad66ff6c2f8718d2a6f3f8f30412d53e40f3a1
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2022-04-12 23:11:26 +0530

    added interface, dependency injection principle

--- SWD-Day6/DataCollection/DataCollection.cs
+++ SWD-Day6/DataCollection/DataCollection.cs
@@ -6,6 +6,8 @@ namespace SWD_Day6
 {    
     public class DataCollection
     {
+        SensorData Data = new SensorData();
+
         Http api = new Http();
         JsonObject obj = new JsonObject();
         string base_URL = "http://192.168.1.10:8080/shapi/";
@@ -16,79 +18,37 @@ namespace SWD_Day6
             return TimeNow;
         }
 
-        // https://json2csharp.com/json-to-csharp
-        public class Parameters
-        {
-            public string type { get; set; }
-            public string state { get; set; }
-            public double power { get; set; }
-            public string id { get; set; }
-            public bool motionDetected { get; set; }
-        }
-
         public string SocketEnergy()
         {
-            string JsonDataNew = ExtractJson("state?identifier=SocketEnergy");
-            Parameters myDeserializedClass = JsonConvert.DeserializeObject<Parameters>(JsonDataNew);
-            string SocketEnergy = myDeserializedClass.power.ToString() + " watt";
-            return SocketEnergy;
+            string result = Data.SocketEnergy();
+            return result;
         }
-
         public string SocketState()
         {
-            string JsonDataNew = ExtractJson("state?identifier=Socket");
-            Parameters myDeserializedClass = JsonConvert.DeserializeObject<Parameters>(JsonDataNew);
-            string SocketState = myDeserializedClass.state.ToString();
-            return SocketState;
+            string result = Data.SocketState();
+            return result;
         }
-
         public string ButtonState()
         {
-            string JsonDataNew = ExtractJson("state?identifier=Button");
-            
-            if(JsonDataNew.Length == 259)
-            {
-                return "RELEASED";
-            }
-            else
-            {
-                Parameters myDeserializedClass = JsonConvert.DeserializeObject<Parameters>(JsonDataNew);
-                string ButtonState = myDeserializedClass.state.ToString();
-                return ButtonState;
-            }            
+            string result = Data.ButtonState();
+            return result;
         }
-
         public string Motion()
         {
-            string JsonDataNew = ExtractJson("state?identifier=Motion");
-            int startIndex = 63;
-            int endIndex = JsonDataNew.Length - startIndex;
-            string MotionJson = JsonDataNew.Substring(startIndex, endIndex);
-            Parameters myDeserializedClass = JsonConvert.DeserializeObject<Parameters>(MotionJson);
-            string Motion = myDeserializedClass.motionDetected.ToString();
-            return Motion;
+            string result = Data.Motion();
+            return result;
         }
-
-        public string ExtractJson(string sensor_url)
-        {
-            string JsonData = api.Get(base_URL + sensor_url).Result;
-            string JsonDataNew = JsonData.Substring(1, JsonData.Length - 2);
-            return JsonDataNew;
-        }
-
-        public string TurnOnSocket()
+        public virtual string TurnOnSocket()
         {
             string result = api.Put(base_URL, obj.SwitchOnSocket).Result;
             result = "Socket Turned ON";
             return result;
         }
-
-        public string TurnOffSocket()
+        public virtual string TurnOffSocket()
         {
             string result = api.Put(base_URL, obj.SwitchOffSocket).Result;
             result = "Socket Turned OFF";
             return result;
         }
-
     }
 }
